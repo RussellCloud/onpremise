8110613d54fcba311c6fe28e32200e007f029d2d
Danceiny <danceiny@gmail.com>
2017-12-21 23:13:10 +0800

    [BUG]

--- start.py
+++ start.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 cmd = ["docker-compose", "run", "--rm", "web", "config", "generate-secret-key"]
 res = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
-sk = res.split('\n')[-2]
+sk = res.strip()[-50:]
 
 filename = 'docker-compose.yml'
 f = open(filename, 'r')
